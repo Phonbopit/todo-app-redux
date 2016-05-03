fc2299e08dcc544103074f2101598047a5fc56b1
Chai Phonbopit <phonbopit@gmail.com>
2016-05-04 00:36:47 +0700

    create test for handle ADD_TODO reducers

--- test/reducers/todo.spec.js
+++ test/reducers/todo.spec.js
@@ -5,6 +5,25 @@ describe('Todo Reducers', () => {
   it('should have default state', () => {
     expect(
       todos(undefined, {})
-    ).to.equal([]);
+    ).to.eql([]);
+  });
+
+  it('should handle ADD_TODO', () => {
+
+    // empty todo, state = []
+    expect(
+      todos([], {
+        type: 'ADD_TODO',
+        text: 'Learn React',
+        id: 0
+      })
+    ).to.eql([
+      {
+        id: 0,
+        text: 'Learn React',
+        completed: false
+      }
+    ]);
+
   });
 });
