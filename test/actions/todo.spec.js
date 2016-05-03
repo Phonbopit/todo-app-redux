9b708c603e55b3b617c2c1088d1a61b4d3a4f843
Chai Phonbopit <phonbopit@gmail.com>
2016-05-04 00:17:37 +0700

    setup actions spec - fixed compare object with to.deep.equal() - use mocha watch option

--- test/actions/todo.spec.js
+++ test/actions/todo.spec.js
@@ -4,7 +4,7 @@ import { addTodo } from '../../actions/todos';
 describe('Todo Actions', () => {
 
   it('addTodo() should create ADD_TODO action', () => {
-    expect(addTodo('Setup React')).to.equal({
+    expect(addTodo('Setup React')).to.deep.equal({
       type: 'ADD_TODO',
       id: 0,
       text: 'Setup React'
