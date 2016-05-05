2de34d9c000a6de6ade0afd32df2a809865c40a8
Chai Phonbopit <phonbopit@gmail.com>
2016-05-05 16:58:17 +0700

    add action for filter

--- actions/todos.js
+++ actions/todos.js
@@ -1,6 +1,5 @@
 let currentId = 0;
 export const addTodo = (text) => {
-
   return {
     type: 'ADD_TODO',
     id: currentId++,
@@ -9,9 +8,15 @@ export const addTodo = (text) => {
 };
 
 export const toggleTodo = (id) => {
-
   return {
     type: 'TOGGLE_TODO',
     id: id
   };
 };
+
+export const setFilter = (filter) => {
+  return {
+    type: 'SET_FILTER',
+    filter
+  }
+}
